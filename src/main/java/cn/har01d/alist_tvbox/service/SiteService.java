391234cdfc073be96b8c030b89b5c6e5c34cb16d
IsayIsee <solin.zhan@gmail.com>
2026-05-09 17:47:30 +0800

    Init

--- src/main/java/cn/har01d/alist_tvbox/service/SiteService.java
+++ src/main/java/cn/har01d/alist_tvbox/service/SiteService.java
@@ -19,6 +19,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Sort;
